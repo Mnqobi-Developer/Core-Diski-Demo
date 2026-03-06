e42fcce7d592898f4a8b21261a313ba14a4c195b
Mnqobi Ntereke <LenkaNtereke25@gmail.com>
2026-03-06 23:41:40 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Core_Diski_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Diski_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
